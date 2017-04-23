8ec7717dc70398ee266b3c0b7fa0d13b67a26b01
houdunwang <2300071698@qq.com>
2017-04-23 19:57:54 +0800

    no message

--- src/RouteProvider.php
+++ src/RouteProvider.php
@@ -7,27 +7,27 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\route;
 
-use houdunwang\config\Config;
 use houdunwang\framework\build\Provider;
 
-class RouteProvider extends Provider {
-	//延迟加载
-	public $defer = false;
+class RouteProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	public function boot() {
-		//解析路由
-		require ROOT_PATH . '/system/routes.php';
-		Route::dispatch();
-	}
+    public function boot()
+    {
+    }
 
-	public function register() {
-		Config::set( 'controller.app', Config::get( 'app.path' ) );
-		Config::set( 'route.cache', Config::get( 'http.route_cache' ) );
-		Config::set( 'route.mode', Config::get( 'http.route_mode' ) );
-		$this->app->single( 'Route', function () {
-			return Route::single();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'Route',
+            function () {
+                return Route::single();
+            }
+        );
+    }
 }
--- src/build/Compile.php
+++ src/build/Compile.php
@@ -14,171 +14,199 @@ use Closure;
 use houdunwang\container\Container;
 use houdunwang\controller\Controller;
 use houdunwang\request\Request;
-use houdunwang\response\Response;
-use houdunwang\view\View;
 
-class Compile extends Setting {
-	//匹配到路由
-	protected $found = false;
-	//路由参数
-	protected $args = [];
-	//匹配成功的路由规则
-	protected $matchRoute = '';
+trait Compile
+{
+    //匹配到路由
+    protected $found = false;
+    //路由参数
+    protected $args = [];
+    //匹配成功的路由规则
+    protected $matchRoute = '';
+    //解析结果
+    protected $result;
 
-	//匹配路由
-	protected function isMatch( $key ) {
-		if ( preg_match( $this->route[ $key ]['regexp'], $this->requestUri ) ) {
-			//获取参数
-			$this->route[ $key ]['get'] = $this->getArgs( $key );
-			//验证参数
-			if ( ! $this->checkArgs( $key ) ) {
-				return false;
-			}
-			//设置GET参数
-			$this->args = $this->route[ $key ]['get'];
-			//匹配成功的路由规则
-			$this->matchRoute = $this->route[ $key ];
+    /**
+     * @return mixed
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
 
-			return $this->found = true;
-		}
-	}
+    //匹配路由
+    protected function isMatch($key)
+    {
+        if (preg_match($this->route[$key]['regexp'], $this->requestUri)) {
+            //获取参数
+            $this->route[$key]['get'] = $this->getArgs($key);
+            //验证参数
+            if ( ! $this->checkArgs($key)) {
+                return false;
+            }
+            //设置GET参数
+            $this->args = $this->route[$key]['get'];
+            //匹配成功的路由规则
+            $this->matchRoute = $this->route[$key];
 
-	//获取请求参数
-	protected function getArgs( $key ) {
-		$args = [];
-		if ( preg_match_all( $this->route[ $key ]['regexp'], $this->requestUri, $matched, PREG_SET_ORDER ) ) {
-			//参数列表
-			foreach ( $this->route[ $key ]['args'] as $n => $value ) {
-				if ( isset( $matched[0][ $n + 1 ] ) ) {
-					$args[ $value[1] ] = $matched[0][ $n + 1 ];
-				}
-			}
-		}
+            return $this->found = true;
+        }
+    }
 
-		return $args;
-	}
+    //获取请求参数
+    protected function getArgs($key)
+    {
+        $args = [];
+        if (preg_match_all($this->route[$key]['regexp'], $this->requestUri,
+            $matched, PREG_SET_ORDER)) {
+            //参数列表
+            foreach ($this->route[$key]['args'] as $n => $value) {
+                if (isset($matched[0][$n + 1])) {
+                    $args[$value[1]] = $matched[0][$n + 1];
+                }
+            }
+        }
 
-	//验证路由参数
-	protected function checkArgs( $key ) {
-		$route = $this->route[ $key ];
-		if ( ! empty( $route['where'] ) ) {
-			foreach ( $route['where'] as $name => $regexp ) {
-				if ( isset( $route['get'][ $name ] ) && ! preg_match( $regexp, $route['get'][ $name ] ) ) {
-					return false;
-				}
-			}
-		}
+        return $args;
+    }
 
-		return true;
-	}
+    //验证路由参数
+    protected function checkArgs($key)
+    {
+        $route = $this->route[$key];
+        if ( ! empty($route['where'])) {
+            foreach ($route['where'] as $name => $regexp) {
+                if (isset($route['get'][$name])
+                    && ! preg_match($regexp, $route['get'][$name])
+                ) {
+                    return false;
+                }
+            }
+        }
 
-	//执行路由事件
-	public function exec( $key ) {
-		//匿名函数
-		if ( $this->route[ $key ]['callback'] instanceof Closure ) {
-			//反射分析闭包
-			$reflectionFunction = new \ReflectionFunction( $this->route[ $key ]['callback'] );
-			$gets               = $this->route[ $key ]['get'];
-			$args               = [];
-			foreach ( $reflectionFunction->getParameters() as $k => $p ) {
-				if ( isset( $gets[ $p->name ] ) ) {
-					//如果GET变量中存在则将GET变量值赋予,也就是说GET优先级高
-					$args[ $p->name ] = $gets[ $p->name ];
-				} else {
-					//如果类型为类时分析类
-					if ( $dependency = $p->getClass() ) {
-						$args[ $p->name ] = Container::build( $dependency->name );
-					} else {
-						//普通参数时获取默认值
-						$args[ $p->name ] = Container::resolveNonClass( $p );
-					}
-				}
-			}
-			$result = $reflectionFunction->invokeArgs( $args );
-			if ( IS_AJAX && is_array( $result ) ) {
-				Response::ajax( $result );
-			} else {
-				echo is_object( $result ) ? View::toString() : $result;
-			}
-		} else {
-			//设置控制器与方法
-			Request::set( 'get.' . Config::get( 'http.url_var' ), $this->route[ $key ]['callback'] );
-			Controller::run( $this->route[ $key ]['get'] );
-		}
-	}
+        return true;
+    }
 
-	//URL事件处理
-	protected function _alias( $key ) {
-		if ( $this->isMatch( $key ) ) {
-			//替换GET参数
-			$url = $this->route[ $key ]['callback'];
-			foreach ( $this->route[ $key ]['get'] as $k => $v ) {
-				$url = str_replace( '{' . $k . '}', $v, $url );
-			}
-			//解析后的GET参数设置到全局$_GET中
-			parse_str( $url, $gets );
-			foreach ( (array) $gets as $k => $v ) {
-				Request::set( 'get.' . $k, $v );
-			}
-			Controller::run( $gets );
-		}
-	}
+    //执行路由事件
+    public function exec($key)
+    {echo 333;
+        //匿名函数
+        if ($this->route[$key]['callback'] instanceof Closure) {
+            //反射分析闭包
+            $reflectionFunction
+                  = new \ReflectionFunction($this->route[$key]['callback']);
+            $gets = $this->route[$key]['get'];
+            $args = [];
+            foreach ($reflectionFunction->getParameters() as $k => $p) {
+                if (isset($gets[$p->name])) {
+                    //如果GET变量中存在则将GET变量值赋予,也就是说GET优先级高
+                    $args[$p->name] = $gets[$p->name];
+                } else {
+                    //如果类型为类时分析类
+                    if ($dependency = $p->getClass()) {
+                        $args[$p->name] = Container::build($dependency->name);
+                    } else {
+                        //普通参数时获取默认值
+                        $args[$p->name] = Container::resolveNonClass($p);
+                    }
+                }
+            }
+            $this->result = $reflectionFunction->invokeArgs($args);
+        } else {
+            //设置控制器与方法
+            Request::set('get.'.Config::get('http.url_var'),
+                $this->route[$key]['callback']);
 
-	//GET事件处理
-	protected function _get( $key ) {
-		return IS_GET && $this->isMatch( $key ) && $this->exec( $key );
-	}
+            Controller::run($this->route[$key]['get']);
+        }
 
-	//POST事件处理
-	protected function _post( $key ) {
-		return IS_POST && $this->isMatch( $key ) && $this->exec( $key );
-	}
+        return true;
+    }
 
-	//PUT事件处理
-	protected function _put( $key ) {
-		return IS_PUT && $this->isMatch( $key ) && $this->exec( $key );
-	}
+    //URL事件处理
+    protected function _alias($key)
+    {
+        if ($this->isMatch($key)) {
+            //替换GET参数
+            $url = $this->route[$key]['callback'];
+            foreach ($this->route[$key]['get'] as $k => $v) {
+                $url = str_replace('{'.$k.'}', $v, $url);
+            }
+            //解析后的GET参数设置到全局$_GET中
+            parse_str($url, $gets);
+            foreach ((array)$gets as $k => $v) {
+                Request::set('get.'.$k, $v);
+            }
+            Controller::run($gets);
+        }
+    }
 
-	//DELETE事件
-	protected function _delete( $key ) {
-		return IS_DELETE && $this->isMatch( $key ) && $this->exec( $key );
-	}
+    //GET事件处理
+    protected function _get($key)
+    {
+        return IS_GET && $this->isMatch($key) && $this->exec($key);
+    }
 
-	//任意提交模式
-	protected function _any( $key ) {
-		return $this->isMatch( $key ) && $this->exec( $key );
-	}
+    //POST事件处理
+    protected function _post($key)
+    {
+        return IS_POST && $this->isMatch($key) && $this->exec($key);
+    }
 
-	//控制器路由
-	protected function _controller( $key ) {
-		if ( $this->route[ $key ]['method'] == 'controller' && $this->isMatch( $key ) ) {
-			//控制器方法
-			$method = $this->getRequestAction() . ucfirst( $this->route[ $key ]['get']['method'] );
-			//从容器提取控制器对象
-			$info = explode( '/', $this->route[ $key ]['callback'] );
-			define( 'MODULE', array_shift( $info ) );
-			define( 'CONTROLLER', array_shift( $info ) );
-			define( 'ACTION', $method );
-			Controller::run();
-		}
-	}
+    //PUT事件处理
+    protected function _put($key)
+    {
+        return IS_PUT && $this->isMatch($key) && $this->exec($key);
+    }
 
-	//获取请求方法
-	public function getRequestAction() {
-		switch ( true ) {
-			case IS_GET:
-				return 'get';
-			case IS_POST:
-				return 'post';
-			case IS_PUT:
-				return 'put';
-			case IS_DELETE:
-				return 'delete';
-		}
-	}
+    //DELETE事件
+    protected function _delete($key)
+    {
+        return IS_DELETE && $this->isMatch($key) && $this->exec($key);
+    }
 
-	//获取解析后的参数
-	public function getArg() {
-		return $this->args;
-	}
+    //任意提交模式
+    protected function _any($key)
+    {
+        return $this->isMatch($key) && $this->exec($key);
+    }
+
+    //控制器路由
+    protected function _controller($key)
+    {
+        if ($this->route[$key]['method'] == 'controller'
+            && $this->isMatch($key)
+        ) {
+            //控制器方法
+            $method = $this->getRequestAction()
+                .ucfirst($this->route[$key]['get']['method']);
+            //从容器提取控制器对象
+            $info = explode('/', $this->route[$key]['callback']);
+            define('MODULE', array_shift($info));
+            define('CONTROLLER', array_shift($info));
+            define('ACTION', $method);
+            Controller::run();
+        }
+    }
+
+    //获取请求方法
+    public function getRequestAction()
+    {
+        switch (true) {
+            case IS_GET:
+                return 'get';
+            case IS_POST:
+                return 'post';
+            case IS_PUT:
+                return 'put';
+            case IS_DELETE:
+                return 'delete';
+        }
+    }
+
+    //获取解析后的参数
+    public function getArg()
+    {
+        return $this->args;
+    }
 }
